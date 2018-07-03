fd039fc6d0b3de72d3edacc3ef5d9d3422db01cb
wingmeng <wingmeng@qq.com>
2018-07-03 14:10:05 +0800

    添加注释

--- sum.js
+++ sum.js
@@ -1,13 +1,15 @@
+/**
+ * 归纳求和
+ * @param {number | array} params - 求和项，支持多参数形式或单数组封装形式传入
+ */
 function sum(params) {
-    var add = function(a, b) {
-        return a + b;
-    };
-
     if (Array.isArray(params)) {
         return params.reduce(function(prev, cur) {
             return prev + cur;
         });
     }
 
-    return Array.prototype.reduce.call(arguments, add);
+    return Array.prototype.reduce.call(arguments, function(a, b) {
+		return a + b;
+	});
 }
