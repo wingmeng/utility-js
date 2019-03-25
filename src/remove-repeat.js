543ee522d78289a4543ae88b75d6f17d63d02105
wingmeng <wingmeng@qq.com>
2019-03-25 11:30:36 +0800

    数组去重

---
+++ src/remove-repeat.js
@@ -0,0 +1,19 @@
+/**
+ * 移除给定数组中的重复元素，返回新数组
+ * @param {Array} arr - 待处理数组
+ */
+function removeRepeat(arr) {
+	var result = [],
+		hash = {};
+
+	for (var i = 0, elm; i < arr.length; i++) {
+		elm = arr[i];
+
+		if (!hash[elm]) {
+			result.push(elm);
+			hash[elm] = true;
+		}
+	}
+
+	return result;
+}
